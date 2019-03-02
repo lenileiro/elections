bbc66ca6d27d7158221f9661d691c0b2aebd0f50
lenileiro <lenileiro@gmail.com>
2019-03-02 16:01:53 +0300

    [Chores 164349576] added heruko configurations

--- backend/run.py
+++ backend/run.py
@@ -3,8 +3,9 @@ import os
 
 from app import create_app
 config_name = os.getenv("FLASK_ENV", 'development')
+runport = int(os.environ.get("PORT", 8050))
 
 app = create_app(config_name)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5002)
+    app.run(host='0.0.0.0', port=runport)
