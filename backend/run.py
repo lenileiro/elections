ec47d78e74a895fa6faadddd3343f12f53b92334
lenileiro <lenileiro@gmail.com>
2019-03-02 13:12:33 +0300

    [Chores 164349576] fixed port address

--- backend/run.py
+++ backend/run.py
@@ -7,4 +7,4 @@ config_name = os.getenv("FLASK_ENV", 'development')
 app = create_app(config_name)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5002)
